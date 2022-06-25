33347ed5adcd0c7a264124c688c901665db503c3
igorevanss <igorevans.pro@gmail.com>
2022-06-25 14:55:47 -0300

    Todos os requisitos prontos

--- script.js
+++ script.js
@@ -3,13 +3,16 @@ let userName
 function joinChat() {
   userName = prompt('Qual seu nome?')
 
-  const promise = axios.post('https://mock-api.driven.com.br/api/v6/uol/participants', {name: userName})
+  const promise = axios.post(
+    'https://mock-api.driven.com.br/api/v6/uol/participants',
+    { name: userName }
+  )
   promise.then(startChat)
   promise.catch(resetPage)
 }
+joinChat()
 
 function startChat() {
-
   bringData()
 
   setInterval(bringData, 3000)
@@ -17,26 +20,38 @@ function startChat() {
 }
 
 function checkIfActive() {
-  axios.post('https://mock-api.driven.com.br/api/v6/uol/status', {name: userName})
+  axios.post('https://mock-api.driven.com.br/api/v6/uol/status', {
+    name: userName
+  })
 }
 
 function resetPage() {
   alert('Este nome de usuário já está em uso, por favor digite outro nome!!')
 
-  window.location.reload();
+  window.location.reload()
+}
+
+function ifUnlogged() {
+  alert(
+    'Você deslogou automaticamente por ficar muito tempo inativo, faça login novamente!'
+  )
+
+  window.location.reload()
 }
 
 function bringData() {
-  const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages')
-  promise.then(renderMessages);
+  const promise = axios.get(
+    'https://mock-api.driven.com.br/api/v6/uol/messages'
+  )
+  promise.then(renderMessages)
 }
 
-function renderMessages(reply){
-  console.log(reply.data)
+function renderMessages(reply) {
+  //console.log(reply.data)
 
-  const msg = document.querySelector('main');
+  const msg = document.querySelector('main')
 
-  msg.innerHTML = '';
+  msg.innerHTML = ''
   for (let i = 0; i < reply.data.length; i++) {
     if (reply.data[i].type === 'status') {
       msg.innerHTML += `
@@ -45,16 +60,21 @@ function renderMessages(reply){
           <span>(${reply.data[i].time})</span><strong>${reply.data[i].from}</strong>${reply.data[i].text}
         </p>
       </div>
-      `;
-    }if (reply.data[i].type === 'private_message') {
+      `
+    }
+    if (
+      reply.data[i].type === 'private_message' &&
+      (reply.data[i].from === userName || reply.data[i].to === userName)
+    ) {
       msg.innerHTML += `
       <div class="private-talk">
         <p>
           <span>(${reply.data[i].time})</span><strong>${reply.data[i].from}</strong>reservadamente para<strong>${reply.data[i].to}:</strong>${reply.data[i].text}
         </p>
       </div>
-      `;
-    }if (reply.data[i].type === 'message') {
+      `
+    }
+    if (reply.data[i].type === 'message') {
       msg.innerHTML += `
       <div class="everyone-talk">
         <p>
@@ -67,10 +87,25 @@ function renderMessages(reply){
   lastMsg()
 }
 
-function lastMsg(){
+function lastMsg() {
   const viewLastMsg = document.querySelector('main div:last-of-type')
 
   viewLastMsg.scrollIntoView()
 }
 
-joinChat()
+function sendMsg() {
+  const text = document.querySelector('footer > input').value
+  const msgModel = {
+    from: userName,
+    to: 'Todos',
+    text: text,
+    type: 'message'
+  }
+  document.querySelector('footer > input').value = ''
+  const promise = axios.post(
+    'https://mock-api.driven.com.br/api/v6/uol/messages',
+    msgModel
+  )
+  promise.then(bringData)
+  promise.catch(ifUnlogged)
+}
